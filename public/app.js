208140382217daa91baa0e3d9c0dde317248e07c
Aaqib Habib <aaqib.habib@gmail.com>
2015-11-09 09:27:42 -0800

    q1 done

--- public/app.js
+++ public/app.js
@@ -1,5 +1,24 @@
-var app = angular.module('App', ['ui.bootstrap']);
+/// <reference path="../typings/tsd.d.ts" />
+(function(){
+	'use strict'
+	
+	var app = angular.module('App', ['ui.bootstrap']);
 
-app.controller('QuestionOne', ['$scope', function($scope) {
-
-}]);
+	app.controller('Q1Ctrl', function($scope, $http) {
+		let baseUrl = '/api/';
+		// Get trips
+		$scope.trips = [];
+		$http.get(baseUrl + 'trips/').then(function(res){
+			$scope.trips = res.data;
+		});
+	});
+	
+	app.controller('Q2Ctrl', function($scope, $http) {
+		let baseUrl = '/api/';
+		// Get trips
+		$scope.trips = [];
+		$http.get(baseUrl + 'trips/').then(function(res){
+			$scope.trips = res.data;
+		});
+	});
+})();
