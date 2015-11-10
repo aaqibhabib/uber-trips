94c6a0ba6845d74426a06afa33c9c519f380dc85
Aaqib Habib <aaqib.habib@gmail.com>
2015-11-09 19:16:26 -0800

    q2 stub

--- public/app.js
+++ public/app.js
@@ -6,18 +6,39 @@
 	var baseUrl = '/api/';
 	
 	app.controller('Q1Ctrl', function($scope, $http) {
+		$scope.maxSize = 10;
+		$scope.currentPage = 1;
+		$scope.totalItems = 0;
+		
 		// Get trips
 		$scope.trips = [];
 		$http.get(baseUrl + 'trips/').then(function(res){
 			$scope.trips = res.data;
+			$scope.totalItems = res.data.length;
 		});
+		
+
 	});
 	
 	app.controller('Q2Ctrl', function($scope, $http) {
-		// Get trips
-		// $scope.trips = [];
-		// $http.get(baseUrl + 'trips/').then(function(res){
-		// 	$scope.trips = res.data;
-		// });
+		$scope.selctedDriver = 1;
+		$scope.selctedPassenger = 1;
+		$scope.selectedHour;
+		
+		$scope.hourRange = new Array(24);
+		
+		$http.get(baseUrl + 'trips/stats/').then(function(res){
+			$scope.stats = res.data;
+			$scope.drivers = res.data.drivers.map(function(driver){
+				return driver['_id'];
+			});
+			// $scope.drivers.unshift('Choose a driver to filter by');
+			
+			$scope.passengers = res.data.passengers.map(function(passenger){
+				return passenger['_id'];
+			});
+			// $scope.passengers.unshift('Choose a passenger to filter by');
+			
+		});
 	});
 })();
